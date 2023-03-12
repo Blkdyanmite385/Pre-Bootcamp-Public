7ae3ec37e6c620b81ea77a6847b3805d40ce661d
damesx <125682627+damesx@users.noreply.github.com>
2023-03-12 15:09:00 -0400

    Create Avery_Rajourn_JSPredict.js

---
+++ Avery_Rajourn_JSPredict.js
@@ -0,0 +1,24 @@
+function myBirthYearFunc(){
+        console.log("I was born in " + 1980);
+    }
+    // console will print "I was born in 1980".
+
+// birthyearinput will be: 1980
+    function myBirthYearFunc(birthYearInput){
+            console.log("I was born in " + birthYearInput);
+        }
+// console log will print "I was born in 1980"
+
+//  calling this funcation with: var num1 = 10 and var num2 = 20
+function add(num1, num2){
+        console.log("Summing Numbers!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+
+// console log will print summing Numbers!
+// console log will print num1 is 10
+// console log will print num2 is 20
+// console log will print sum (30)
